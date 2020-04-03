2509b1b5efed6c5fb6dbdb30b33d49f9364d7e34
Marton Szuts <mszuts@pc-428.home>
2020-04-03 22:54:21 +0200

    pathfinding errors fixed

--- src/pathfinding/mob_path.c
+++ src/pathfinding/mob_path.c
@@ -22,19 +22,19 @@ void search_mob_path(mob_t *node, all_t *s_all)
 {
     int x = node->x, y = node->y;
     if (node->path == NULL) return;
-    if (node->y != 0) {
-        if (node->y != 0 && node->path[y - 1][x] == ' ' && node->prev != 'D')
-            move_mob_up(node, s_all);
-        }
-    if (node->path[y][x + 1] == ' ' && node->prev != 'L')
+    if (node->y != 0 && node->path[y - 1][x] == ' ' && node->prev != 'D') {
+        move_mob_up(node, s_all);
+        return;
+    }
+    if (node->path[y][x + 1] == ' ' && node->prev != 'L') {
         move_mob_right(node, s_all);
-    if (node->y != s_all->s_map.y - 1) {
-        if (node->y != s_all->s_map.y - 1 && node->path[y + 1][x] == ' ' &&
-        node->prev != 'U')
-            move_mob_down(node, s_all);
+        return;
     }
-    if (node->x != 0) {
-        if (node->x != 0 && node->path[y][x - 1] == ' ' && node->prev != 'R')
-            move_mob_left(node, s_all);
+    if (node->y != s_all->s_map.y - 1 && node->path[y + 1][x] == ' ' &&
+    node->prev != 'U') {
+        move_mob_down(node, s_all);
+        return;
     }
+    if (node->x != 0 && node->path[y][x - 1] == ' ' && node->prev != 'R')
+        move_mob_left(node, s_all);
 }
