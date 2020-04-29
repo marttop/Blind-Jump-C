b032424cbf0f1ea1db6b2f0433d4aa208cd291d8
Lorentz ROMEO <lorentz-andrea.romeo@epitech.eu>
2020-04-29 13:54:55 +0200

    Fixed turret bullet range and gun color

--- src/turret2.c
+++ src/turret2.c
@@ -12,7 +12,7 @@ void turret_loop(all_t *s_all, mob_t *temp)
     b_mob_t *bullet = temp->l_bullets;
     for (int i = 0; bullet != NULL; bullet = bullet->next, i++) {
         turret_aim(i, temp, bullet);
-        if (temp->bullet_travel >= 450) {
+        if (temp->bullet_travel >= 650) {
             b_mob_t *reset = temp->l_bullets;
             for (; reset != NULL; reset = reset->next) {
                 reset->check_shoot = 0, reset->hit = 0;
@@ -20,7 +20,7 @@ void turret_loop(all_t *s_all, mob_t *temp)
                 (sfVector2f){temp->mob_pos.x, temp->mob_pos.y - 10});
             }
         }
-    if (temp->bullet_travel >= 450)
+    if (temp->bullet_travel >= 650)
         temp->bullet_travel = 0, temp->shoot = 0, temp->check_shoot = 0;
     if (temp->shoot == 1 && bullet->hit != 1)
         sfRenderWindow_drawSprite(s_all->s_game.window,
