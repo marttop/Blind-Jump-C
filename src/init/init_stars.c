efcbfe25cbb343cb62220303f962462175c7c347
Lorentz ROMEO <lorentz-andrea.romeo@epitech.eu>
2020-04-29 17:24:49 +0200

    Fixed a bit the turret and finished the stars

--- src/init/init_stars.c
+++ src/init/init_stars.c
@@ -10,17 +10,17 @@
 void stars_create_texture(all_t *s_all, sfSprite *sprite,
     sfRenderTexture *render)
 {
-    render = sfRenderTexture_create(128 * 30, 128 * 30, 0);
+    render = sfRenderTexture_create(128 * 50, 128 * 50, 0);
     s_all->s_stars.pos = (sfVector2f){0, 0};
-    for (int i = 0, j = 0; i != 30; i++, s_all->s_stars.pos.y += 128,
+    for (int i = 0, j = 0; i != 50; i++, s_all->s_stars.pos.y += 128,
     s_all->s_stars.pos.x = 0) {
-        for (j = 0; j != 30; j++, s_all->s_stars.pos.x += 128) {
+        for (j = 0; j != 50; j++, s_all->s_stars.pos.x += 128) {
         sfSprite_setPosition(sprite, s_all->s_stars.pos);
         sfRenderTexture_drawSprite(render, sprite, NULL);
         }
     }
     sfSprite_setTexture(sprite, sfRenderTexture_getTexture(render), sfTrue);
-    s_all->s_stars.pos = (sfVector2f){-128 * 5, -128 * 5};
+    s_all->s_stars.pos = (sfVector2f){-128 * 15, -128 * 15};
     sfSprite_setPosition(sprite, s_all->s_stars.pos);
 }
 
@@ -38,30 +38,22 @@ void init_stars(all_t *s_all)
     stars_create_texture(s_all, s_all->s_stars.front, s_all->s_stars.render2);
 }
 
-//void move_stars_diag(all_t *s_all, sfSprite *sprite)
-//{
-//    if (s_all->s_player.hero_seconds < 0.01 || s_all->s_player.tp == 1) return;
-//}
-
 void move_stars(all_t *s_all, sfSprite *sprite)
 {
-    if (s_all->s_player.hero_seconds < 0.01 || s_all->s_player.tp == 1) return;
+    if (s_all->s_player.hero_seconds < 0.01 || s_all->s_player.tp == 1
+    || s_all->s_game.pause == 1 || (s_all->s_player.down == 1
+    && s_all->s_player.up == 1) || (s_all->s_player.left == 1
+    && s_all->s_player.right == 1)) return;
     if (s_all->s_movement.left == 1 && s_all->s_movement.wall_left == 0) {
-        //if (s_all->s_movement.up == 1 || s_all->s_movement.down == 1) return;
         s_all->s_stars.pos.x -= 1;
         sfSprite_setPosition(sprite, s_all->s_stars.pos);
     } if (s_all->s_movement.down == 1 && s_all->s_movement.wall_down == 0) {
-        //if (s_all->s_movement.left == 1 || s_all->s_movement.right == 1)
-        //    return;
         s_all->s_stars.pos.y += 1;
         sfSprite_setPosition(sprite, s_all->s_stars.pos);
     } if (s_all->s_movement.right == 1 && s_all->s_movement.wall_right == 0) {
-        //if (s_all->s_movement.up == 1 || s_all->s_movement.down == 1) return;
         s_all->s_stars.pos.x += 1;
         sfSprite_setPosition(sprite, s_all->s_stars.pos);
     } if (s_all->s_movement.up == 1 && s_all->s_movement.wall_up == 0) {
-        //if (s_all->s_movement.left == 1 || s_all->s_movement.right == 1)
-        //    return;
         s_all->s_stars.pos.y -= 1;
         sfSprite_setPosition(sprite, s_all->s_stars.pos);
     }
