2408e7f440aed07cc04d87e18e70296bd341f34e
Lionel Silva <lionel.da-rocha-da-silva@epitech.eu>
2020-04-30 20:47:28 +0200

    adding statbox

--- src/display.c
+++ src/display.c
@@ -58,8 +58,10 @@ void display_hud_anim(all_t *s_all)
     if (s_all->s_game.scene != GAME_OVER && s_all->s_game.scene != CUSTOM
     && s_all->s_game.scene != OPT && s_all->s_game.scene != LOAD) {
         display_infos(s_all);
-        if (s_all->s_game.display_inv == 1)
+        if (s_all->s_game.display_inv == 1) {
             display_inventory(s_all);
+            draw_stats(s_all);
+        }
     }
 }
 
