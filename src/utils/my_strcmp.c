1eb6f81b79ac5b75818c4d2addbe483d5f8d2bff
Marton Szuts <marton.szuts@epitech.eu>
2020-05-01 17:07:54 +0200

    warnings

--- src/utils/my_strcmp.c
+++ src/utils/my_strcmp.c
@@ -7,7 +7,7 @@
 
 #include "utils.h"
 
-int my_strcmp(char const *s1, char const *s2)
+int my_strcmp(const char *s1, char *s2)
 {
     for (int i = 0; s1[i] != '\0'; i++) {
         if (s1[i] < s2[i])
