8ea34c7ed6a528fa38661ae6f0c7ff8d9ce52956
Lorentz ROMEO <lorentz-andrea.romeo@epitech.eu>
2020-05-03 23:16:00 +0200

    Should be the final version of the game

--- src/create_entities.c
+++ src/create_entities.c
@@ -15,3 +15,32 @@ void create_sprite(sfSprite **sprite, sfTexture **texture,
     sfSprite_setTexture(*sprite, *texture, sfTrue);
     sfSprite_setPosition(*sprite, pos);
 }
+
+void quest_display(all_t *s_all)
+{
+    if (s_all->s_map.stage == 5 && s_all->s_cine.script < 7)
+        sfText_setString(s_all->s_game.quest, "KILL EVERY THREATS");
+    else if (s_all->s_map.stage < 5 && s_all->s_cine.script < 7)
+        sfText_setString(s_all->s_game.quest, "REACH THE STAGE 5");
+    else sfText_setString(s_all->s_game.quest, "INFINITE MOD");
+}
+
+void set_volume(all_t *s_all)
+{
+    sfMusic_setVolume(s_all->s_sounds.music, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.blast, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.creak, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.dead, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.heart, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.hit, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.item, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.laser, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.lvl, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.player, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.step1, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.step2, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.step3, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.step4, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.step5, s_all->s_game.volume);
+    sfSound_setVolume(s_all->s_sounds.tp, s_all->s_game.volume);
+}
